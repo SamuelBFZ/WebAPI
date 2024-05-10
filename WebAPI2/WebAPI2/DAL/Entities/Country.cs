599f089dc26fc49ac30156f0cb48f6ee87d309ff
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2024-05-09 22:25:03 -0500

    Country Class created

---
+++ WebAPI2/WebAPI2/DAL/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI2.DAL.Entities
+{
+    public class Country : AuditBase
+    {
+        [MaxLength(50, ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Pais")]
+        public string Name { get; set; }
+
+    }
+}
