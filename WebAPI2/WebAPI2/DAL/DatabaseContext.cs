daf87f009f845955c3d90db0dd3e2df38ecd7669
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2024-05-09 22:32:23 -0500

    Database configuration created

---
+++ WebAPI2/WebAPI2/DAL/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using WebAPI2.DAL.Entities;
+using WebAPI2.DAL.Entities;
+
+namespace WebAPI2.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)//conexion a la BD
+        {
+
+        }
+
+        //Metodo propio de EF Core que permite configurar indice de cada campo de una tabla en la BD
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();//No permite que hayan nombres repetidos
+        }
+
+
+        #region DBsets
+        //Cada DbSet significa una tabla en la BD
+        public DbSet<Country> Countries { get; set; }
+        #endregion
+    }
+}
+
--- WebAPI2/WebAPI2/Program.cs
+++ WebAPI2/WebAPI2/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI2.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Codigo para configurar conexión a la base de datos
+builder.Services.AddDbContext<DatabaseContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
