6ee573ac1e6429451392f2737d664fdb7390dd0a
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-27 16:50:53 -0500

    Barber Service created

---
+++ WebAPI/WebAPI/Domain/Interfaces/IBarberService.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.DAL.Entities;
+
+namespace WebAPI.Domain.Interfaces
+{
+    public interface IBarberService
+    {
+        Task<IEnumerable<Barber>> GetBarbersAsync();
+        Task<Barber> GetBarberByIdAsync(Guid id);
+        Task<Barber> CreateBarberAsync(Barber barber);
+        Task<Barber> UpdateBarberAsync(Barber barber);
+        Task<Barber> DeleteBarberAsync(Guid id);
+    }
+}
