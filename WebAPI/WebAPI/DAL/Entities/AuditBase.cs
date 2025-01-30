8991b0944680a5660416adcb40cb753f3a570243
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-30 18:50:24 -0500

    Changes in AuditBase class

--- WebAPI/WebAPI/DAL/Entities/AuditBase.cs
+++ WebAPI/WebAPI/DAL/Entities/AuditBase.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace WebAPI.DAL.Entities
 {
     public class AuditBase
     {
         [Key]
-        [Required]
+        [JsonIgnore]
         public virtual Guid Id {  get; set; }
+        [JsonIgnore]
         public virtual DateTime? CreatedDate { get; set; } //para guardar todo registro nuevo con su date
+        [JsonIgnore]
         public virtual DateTime? ModifiedDate { get; set; } //para guardar todo registro que se modificó con su date
     }
 }
---
+++ WebAPI/WebAPI/Migrations/20250130174102_GuidToId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    public partial class GuidToId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Guid",
+                table: "Barbers",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Barbers",
+                newName: "Guid");
+        }
+    }
+}
